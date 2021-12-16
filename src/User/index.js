3e9e768d722c493a2cdbe2a127480d2403f53256
adityachandrafb <adityachandrafb@gmail.com>
2021-12-16 16:02:35 +0700

    new user login
    first commit user login

---
+++ src/User/index.js
@@ -0,0 +1,7 @@
+export {default as UserLogin} from './UserLogin';
+export {default as UserRegister} from './UserRegister';
+export {default as Beranda} from './Beranda';
+export {default as SyaratVaksin} from './SyaratVaksin';
+export {default as Informasi} from './Informasi';
+export {default as Tentang} from './Tentang';
+export {default as UserNotFound} from './UserNotFound';
